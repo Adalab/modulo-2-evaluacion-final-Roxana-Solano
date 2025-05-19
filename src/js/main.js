aad869b7aa6120714420612d58d34e9eb017493e
Roxana Solano <roxansolan@gmail.com>
2025-05-19 19:11:11 +0200

    I add the API

--- src/js/main.js
+++ src/js/main.js
@@ -3,3 +3,16 @@
 const inputSearch = document.querySelector(".js_inputSearch");
 const buttonSerch = document.querySelector(".js_bntSerch");
 const buttonReset = document.querySelector(".js_bntReset");
+const seriesList = document.querySelector(".js_serieslist");
+
+buttonSerch.addEventListener("click", () => {});
+
+function requestApi(name) {
+  fetch(`https://api.jikan.moe/v4/anime?q=${name}`)
+    .then((response) => response.json())
+    .then((data) => renderData(data.data));
+}
+function renderData(data) {
+  console.log(data);
+}
+requestApi();
