faab3762c692e3bd8261a0026ebdcf745cf4a495
Roxana Solano <roxansolan@gmail.com>
2025-05-20 12:24:24 +0200

    I add a favorites section

--- src/js/main.js
+++ src/js/main.js
@@ -4,9 +4,12 @@ const buttonSerch = document.querySelector(".js_bntSerch");
 const buttonReset = document.querySelector(".js_bntReset");
 const seriesList = document.querySelector(".js_serieslist");
 const inputSearch = document.querySelector(".js_inputSearch");
+const seriesFavorite = document.querySelector(".js_seriesfavorite");
+const favorite = [];
 
 //escucho el click del boton buscar
-buttonSerch.addEventListener("click", () => {
+buttonSerch.addEventListener("click", (ev) => {
+  ev.preventDefault();
   const valueInputSearch = inputSearch.value;
   if (valueInputSearch === "") {
     seriesList.innerHTML = `<p> No hay resultados </p>`;
@@ -24,24 +27,35 @@ function requestApi(name) {
 function renderData(data) {
   seriesList.innerHTML = ""; //limpia la lista
   console.log(data);
-  //haces el for adentro del for llamas a draw
+
   for (const anime of data) {
     const title = anime.title;
     const image = anime.images.jpg.image_url;
-    draw(image, title);
+    const id = anime.mal_id;
+    draw(image, title, id, seriesList);
+    addEventSerie(image, title, id, seriesFavorite);
   }
 }
 
-function draw(image, title) {
-  const serie = `<li>
+function draw(image, title, id, container) {
+  const serie = `<li id="serie-${id}">
                     <h2>${title}</h2>
                     <img src="${image}" alt="${title}" width="150">
                 </li>`;
   //innerHTMl del LI seriesList.innerHTML = seriesList.innerHTML + serie
-  seriesList.innerHTML += serie;
+  container.insertAdjacentHTML("beforeend", serie);
 }
 
-buttonReset.addEventListener("click", () => {
+function addEventSerie(image, title, id, seriesFavorite) {
+  const item = document.querySelector(`#serie-${id}`);
+  item.addEventListener("click", (ev) => {
+    ev.preventDefault();
+    draw(image, title, id, seriesFavorite);
+  });
+}
+
+buttonReset.addEventListener("click", (ev) => {
+  ev.preventDefault();
   seriesList.innerHTML = "";
   inputSearch.innerHTML = "";
 });
