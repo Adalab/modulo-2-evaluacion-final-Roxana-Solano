dbd51514a24f541c58e5d497065317d64d3dd8c7
Roxana Solano <roxansolan@gmail.com>
2025-05-20 09:37:57 +0200

    search for anime and display results on screen

--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,42 @@
 "use strict";
 
-const inputSearch = document.querySelector(".js_inputSearch");
 const buttonSerch = document.querySelector(".js_bntSerch");
 const buttonReset = document.querySelector(".js_bntReset");
 const seriesList = document.querySelector(".js_serieslist");
 
-buttonSerch.addEventListener("click", () => {});
+//escucho el click del boton buscar
+buttonSerch.addEventListener("click", () => {
+  const inputSearch = document.querySelector(".js_inputSearch");
+  const valueInputSearch = inputSearch.value;
+  if (valueInputSearch === "") {
+    seriesList.innerHTML = `<p> No hay resultados </p>`;
+  } else {
+    requestApi(valueInputSearch);
+  }
+});
 
 function requestApi(name) {
   fetch(`https://api.jikan.moe/v4/anime?q=${name}`)
     .then((response) => response.json())
     .then((data) => renderData(data.data));
 }
+
 function renderData(data) {
+  seriesList.innerHTML = ""; //limpia la lista
   console.log(data);
+  //haces el for adentro del for llamas a draw
+  for (const anime of data) {
+    const title = anime.title;
+    const image = anime.images.jpg.image_url;
+    draw(image, title);
+  }
 }
-requestApi();
+
+function draw(image, title) {
+  const serie = `<li>
+                    <h2>${title}</h2>
+                    <img src="${image}" alt="${title}" width="150">
+                </li>`;
+  //innerHTMl del LI seriesList.innerHTML = seriesList.innerHTML + serie
+  seriesList.innerHTML += serie;
+}
